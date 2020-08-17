6144376c0747d8ae59fc7793919abde873baae50
makecodework <makecodework@gmail.com>
2020-08-17 16:02:50 +0300

    I/O and serialization

--- task19/Methods.java
+++ task19/Methods.java
@@ -1,26 +1,29 @@
 package task19;
-
-
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class Methods implements Serializable{
 
     public void serialize(Object o) throws IOException {
 
-        FileOutputStream fn = new FileOutputStream("text.txt");
+        FileOutputStream fn = new FileOutputStream("src/text.txt");
         ObjectOutputStream out = new ObjectOutputStream(fn);
 
         out.writeObject(o);
 
-        out.close();
 
         System.out.println("Serialized Successfully");
+
+        out.close();
 
         fn.close();
     }
     public void deserialize(Object o) throws IOException, ClassNotFoundException{
 
-        ObjectInputStream input = new ObjectInputStream(new FileInputStream("text.txt"));
+        ObjectInputStream input = new ObjectInputStream(new FileInputStream("src/text.txt"));
 
         o = input.readObject();
 
